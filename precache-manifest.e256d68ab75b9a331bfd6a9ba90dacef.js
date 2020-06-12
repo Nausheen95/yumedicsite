b11bf17c092921f0e19b1b45c3993540cb5fcbae
Nausheen95 <55658909+Nausheen95@users.noreply.github.com>
2020-06-13 00:12:22 +0530

    First upload

---
+++ precache-manifest.e256d68ab75b9a331bfd6a9ba90dacef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a620991a7a7c0ee3c57cc78358aa7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1b5050498d72736fe1",
+    "url": "/static/css/main.b85f2f40.chunk.css"
+  },
+  {
+    "revision": "a7eab922b78d8faf67dd",
+    "url": "/static/js/2.7ca2331b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ca2331b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1b5050498d72736fe1",
+    "url": "/static/js/main.e5b853f1.chunk.js"
+  },
+  {
+    "revision": "ab6dcb4b45ae4ce6db65",
+    "url": "/static/js/runtime-main.fa5dbb85.js"
+  }
+]);
